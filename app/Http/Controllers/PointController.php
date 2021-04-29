90502850ac7801ea555aa59e87a163996f429d6f
smartdevello <77758615+smartdevello@users.noreply.github.com>
2021-04-29 09:59:26 -0500

    c

--- app/Http/Controllers/PointController.php
+++ app/Http/Controllers/PointController.php
@@ -14,11 +14,23 @@ class PointController extends Controller
     public function index()
     {
         //
+//        $ch = curl_init();
+
+//        curl_setopt($ch, CURLOPT_URL, 'https://172.21.8.245:8000/assemblin/points/byid');
+//        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+//        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
         $ch = curl_init();
 
-        curl_setopt($ch, CURLOPT_URL, 'https://172.21.8.245:8000/assemblin/points/byid');
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+        curl_setopt_array($ch, array(
+            CURLOPT_URL => 'https://172.21.8.245:8000/assemblin/points/byid',
+            CURLOPT_RETURNTRANSFER => true,
+            CURLOPT_ENCODING => '',
+            CURLOPT_MAXREDIRS => 10,
+            CURLOPT_TIMEOUT => 0,
+            CURLOPT_FOLLOWLOCATION => true,
+            CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+            CURLOPT_CUSTOMREQUEST => 'GET',
+        ));
 
         $result = curl_exec($ch);
         if (curl_errno($ch)) {
