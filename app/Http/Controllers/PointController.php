cfc19a475fd0b25de0f9e7693c8102daa2dc294c
smartdevello <77758615+smartdevello@users.noreply.github.com>
2021-04-29 09:28:17 -0500

    point data

--- app/Http/Controllers/PointController.php
+++ app/Http/Controllers/PointController.php
@@ -30,7 +30,7 @@ class PointController extends Controller
         $response = curl_exec($curl);
 
         curl_close($curl);
-        return $response;
+        return json_decode($response);
     }
 
     /**
