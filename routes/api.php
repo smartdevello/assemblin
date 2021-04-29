6609c8571832f84a0afa4c66c1bdee2c47105bf5
smartdevello <77758615+smartdevello@users.noreply.github.com>
2021-04-29 09:24:48 -0500

    aa

--- routes/api.php
+++ routes/api.php
@@ -17,4 +17,4 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
-Route::resource('points', '\App\Http\Controllers\PointController@index');
+Route::resource('points', '\App\Http\Controllers\PointController');
