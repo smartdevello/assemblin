005a25634c73403b4e2c7a7130c709d88dc626ad
smartdevello <77758615+smartdevello@users.noreply.github.com>
2021-04-29 09:17:58 -0500

    aa

--- app/Http/Controllers/PointController.php
+++ app/Http/Controllers/PointController.php
@@ -6,29 +6,59 @@ use Illuminate\Http\Request;
 
 class PointController extends Controller
 {
-    //
-    public function index(){
-
-    }
-    public function getpoints()
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
     {
-        $curl = curl_init();
+        //
+        return ['all' => 1];
+    }
 
-        curl_setopt_array($curl, array(
-          CURLOPT_URL => 'https://172.21.8.245:8000/assemblin/points/byid',
-          CURLOPT_RETURNTRANSFER => true,
-          CURLOPT_ENCODING => '',
-          CURLOPT_MAXREDIRS => 10,
-          CURLOPT_TIMEOUT => 0,
-          CURLOPT_FOLLOWLOCATION => true,
-          CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-          CURLOPT_CUSTOMREQUEST => 'GET',
-        ));
-        
-        $response = curl_exec($curl);
-        
-        curl_close($curl);
-        return response()->json($response, 200);
-        // return $response;
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -17,8 +17,4 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
-
-Route::get('points', 'PointController@getpoints');
-Route::get('test', function(){
-    return ['test' => 1];
-});
+Route::resource('points', 'App\Http\Controllers\PointController@index');
