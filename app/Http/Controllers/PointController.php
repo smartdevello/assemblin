fd035348ea3a9bee5f587c382370f6126d275c30
smartdevello <77758615+smartdevello@users.noreply.github.com>
2021-04-29 09:26:07 -0500

    aa

--- app/Http/Controllers/PointController.php
+++ app/Http/Controllers/PointController.php
@@ -14,7 +14,23 @@ class PointController extends Controller
     public function index()
     {
         //
-        return ['all' => 1];
+        $curl = curl_init();
+
+        curl_setopt_array($curl, array(
+            CURLOPT_URL => 'https://172.21.8.245:8000/assemblin/points/byid',
+            CURLOPT_RETURNTRANSFER => true,
+            CURLOPT_ENCODING => '',
+            CURLOPT_MAXREDIRS => 10,
+            CURLOPT_TIMEOUT => 0,
+            CURLOPT_FOLLOWLOCATION => true,
+            CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+            CURLOPT_CUSTOMREQUEST => 'GET',
+        ));
+
+        $response = curl_exec($curl);
+
+        curl_close($curl);
+        return $response;
     }
 
     /**
