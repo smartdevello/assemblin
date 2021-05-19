3a7bb53d3e350cdc6830e965a93534aa7725d3a6
smartdevello <77758615+smartdevello@users.noreply.github.com>
2021-05-18 20:49:58 -0500

    aaa

--- database/migrations/2021_05_18_211046_deos_points.php
+++ database/migrations/2021_05_18_211046_deos_points.php
@@ -17,6 +17,12 @@ class DeosPoints extends Migration
         Schema::create('deos_points', function (Blueprint $table) {
             $table->id();
             $table->string('name')->nullable();
+            $table->string('label')->nullable();
+            $table->string('type')->nullable();
+            $table->string('meta_property')->nullable();
+            $table->string('meta_room')->nullable();
+            $table->string('meta_sensor')->nullable();
+
             $table->string('value')->nullable();
             $table->unsignedBigInteger('controller_id')->nullable();
             $table->foreign('controller_id')->references('id')->on('deos_controllers')->onDelete('cascade');
