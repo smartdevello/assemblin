9ece02be975dda691b3fdd7ffe796cda2b8e4151
smartdevello <77758615+smartdevello@users.noreply.github.com>
2021-05-23 23:19:24 -0500

    aaa

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -198,7 +198,7 @@
                             point = this.points.find(point => point.id == sensor.point_id);
                             submitdata.push({
                                 "id": point.name,
-                                "value": sensor.value
+                                "value": String(sensor.value)
                             });
                         }
                     }
@@ -211,9 +211,7 @@
                             },
                             "data": JSON.stringify(submitdata),
                     };
-                    console.log(submitdata);
-                    console.log(this.send_data_url);
-                    
+
                     $.ajax(settings).done(function(response) {
                             console.log(response);
                     }).fail(function(jqXHR, textStatus, errorThrown) {
