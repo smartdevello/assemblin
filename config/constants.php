18d951f8ad0f7a88334d9879cac93c97c1e814de
smartdevello <smartdevello@outlook.com>
2021-06-07 10:45:05 +0000

    abc

--- config/constants.php
+++ config/constants.php
@@ -1,16 +1,6 @@
 <?php
-<<<<<<< HEAD
-    return [
-        'BASE_CONFIG_PATH' => "/mnt/assemblin/",
-        'BASE_URL' => 'http://hkasrv4.hameenkiinteistoautomaatio.fi/',
-        'prefix_url' => ''
-    ];
-
-?>
-=======
 return [
-    'BASE_CONFIG_PATH' => "C:\\xampp\\htdocs\\assemblin\\public\\assemblin_config\\",
+    'BASE_CONFIG_PATH' => "/mnt/assemblin/",
     'base_url' => 'http://hkasrv4.hameenkiinteistoautomaatio.fi',
     'prefix_url' => '',
 ];
->>>>>>> ece86fcd5c2ef6f5cefa84dd52a02fc5bcf55f89
