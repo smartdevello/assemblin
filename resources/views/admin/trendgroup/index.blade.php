88f3901e65675801eb68b75e47dc0cdc9f935fbe
smartdevello <77758615+smartdevello@users.noreply.github.com>
2021-09-01 00:35:57 -0500

    adsf

--- resources/views/admin/trendgroup/index.blade.php
+++ resources/views/admin/trendgroup/index.blade.php
@@ -76,8 +76,12 @@
                                     </v-card-title>
 
                                     <v-text-field name="controller_id" label="Controller ID (in HEX)" required class="pa-2" :rules="[ v => !!v || 'Field is required', ]"></v-text-field>
-                                    <v-text-field name="trend_group_name" label="Trend Group Name" required class="pa-2" :rules="[ v => !!v || 'Field is required', ]"></v-text-field>                                    
-{{-- 
+                                    <v-text-field name="trend_group_name" label="Trend group name" required class="pa-2" :rules="[ v => !!v || 'Field is required', ]"></v-text-field>                                    
+                                    <v-text-field name="location_name" label="Location name" required class="pa-2" :rules="[ v => !!v || 'Field is required', ]"></v-text-field>                                    
+                                    <v-text-field name="update_interval" label="Update interval" required class="pa-2" :rules="[ v => !!v || 'Field is required', ]"></v-text-field>                                    
+                                    <v-text-field name="query_period" label="Query period" required class="pa-2" :rules="[ v => !!v || 'Field is required', ]"></v-text-field>                                    
+
+{{--                                     <v-text-field name="trend_group_name" label="Trend Group Name" required class="pa-2" :rules="[ v => !!v || 'Field is required', ]"></v-text-field>                                    
                                     <v-select :items="locations" label="Select A Location" name="location_id" item-text="name" item-value="id" solo required>
                                     </v-select> --}}
                                     <v-card-actions>
