a00caaa1c3d2d66d66d2e222cbbef0b3e0cd7c4e
smartdevello <77758615+smartdevello@users.noreply.github.com>
2021-09-01 20:15:13 -0500

    adf

--- app/Models/Csv_Trend_Data.php
+++ app/Models/Csv_Trend_Data.php
@@ -9,6 +9,7 @@ use Illuminate\Notifications\Notifiable;
 class Csv_Trend_Data extends Model
 {
     use HasFactory, Notifiable;
+    protected $table = 'csv_trend_data';
     protected $fillable = [
         'trend_group_id', 'timestamp', 'sensor_name', 'sensor_value'
     ];
