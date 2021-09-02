06b603f77558cf8b61bf7476bb0fe3b3b06fbf2d
smartdevello <77758615+smartdevello@users.noreply.github.com>
2021-09-02 10:13:53 -0500

    asdf

--- database/migrations/2021_09_01_043113_create_trend_groups_table.php
+++ database/migrations/2021_09_01_043113_create_trend_groups_table.php
@@ -19,7 +19,7 @@ class CreateTrendGroupsTable extends Migration
             $table->string('controller_id')->nullable();
             $table->string('trend_group_name')->nullable();
             $table->string('location_name')->nullable();
-            $table->unsignedBigInteger('update_interval')->nullable();
+            $table->bigInteger('update_interval')->nullable();
             $table->bigInteger('query_period')->nullable();
             $table->timestamps();
         });
