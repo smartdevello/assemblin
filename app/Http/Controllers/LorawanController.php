a5b2d71f2e5eb6049eca13b8a763ae71a7b540a4
smartdevello <77758615+smartdevello@users.noreply.github.com>
2021-09-02 13:41:56 -0500

    asdf

--- app/Http/Controllers/LorawanController.php
+++ app/Http/Controllers/LorawanController.php
@@ -401,8 +401,17 @@ class LorawanController extends Controller
     {
         //
     }
-    public function receive_csvfile($controller_id, $trend_group_name)
+    public function receive_csvfile()
     {
+        if (!isset($_GET['controller_id']) || !isset($_GET['trend_group_name']) ) 
+            return response()->json([
+                'error' => 'Parameters are missing'
+            ], 401);
+
+
+        $controller_id = $_GET['controller_id'];
+        $trend_group_name = $_GET['trend_group_name'];
+
         $filename = "myfile.csv";
         $format = "lynx --dump 'http://172.21.8.245/COSMOWEB?TYP=REGLER&MSG=GET_TRENDVIEW_DOWNLOAD_CVS&COMPUTERNR=THIS&REGLERSTRANG=%s&REZEPT=%s&FROMTIME=%d&TOTIME=%d&' > " . $filename ;
         $to_time = time();
