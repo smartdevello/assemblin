24e4363c627b6ae2abb2f56d228f683f61030b35
smartdevello <77758615+smartdevello@users.noreply.github.com>
2021-09-15 20:49:45 -0500

    asdfsd

--- routes/web.php
+++ routes/web.php
@@ -101,6 +101,12 @@ Route::post('/tokens/create', function (Request $request) {
     $user = $request->user();    
     $token = $user->createToken($request->token_name);
     $plainToken = $token->plainTextToken;
+
+    $token_id = $token->id;
+    return [
+        'id' => $token_id, 
+        'plainToken' => $plainToken
+    ];
     return redirect()->route('setting_index');
 });
 
