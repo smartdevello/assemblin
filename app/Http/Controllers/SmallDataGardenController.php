f9265c778c1e01e87f9a626dec63b34f20dfbafb
Smart Dev <smartdevello@outlook.com>
2022-06-02 14:44:54 -0400

    arqetyasd

--- app/Http/Controllers/SmallDataGardenController.php
+++ app/Http/Controllers/SmallDataGardenController.php
@@ -7,12 +7,14 @@ use Illuminate\Http\Request;
 class SmallDataGardenController extends Controller
 {
     //
+    public $api_baseurl = 'https://smalldata.fi/v2/';
+    public $api_token = 'ZN1hMXTYB6kKE2Dsa7djYPFqM451e7OV';
     public function getAlldevices()
     {
         $curl = curl_init();
 
         curl_setopt_array($curl, array(
-        CURLOPT_URL => 'https://smalldata.fi/v2/devices',
+        CURLOPT_URL => $this->api_baseurl  . 'devices',
         CURLOPT_RETURNTRANSFER => true,
         CURLOPT_ENCODING => '',
         CURLOPT_MAXREDIRS => 10,
@@ -21,7 +23,7 @@ class SmallDataGardenController extends Controller
         CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
         CURLOPT_CUSTOMREQUEST => 'GET',
         CURLOPT_HTTPHEADER => array(
-            'X-Sdg-Token: ZN1hMXTYB6kKE2Dsa7djYPFqM451e7OV'
+            'X-Sdg-Token: ' . $this->api_token
         ),
         ));
 
@@ -31,12 +33,46 @@ class SmallDataGardenController extends Controller
         return json_decode($response);
     }
 
+    public function getDeviceData($deviceId)
+    {
+        $curl = curl_init();
+
+        curl_setopt_array($curl, array(
+        CURLOPT_URL => $this->api_baseurl . `/devices/${deviceId}/data`,
+        CURLOPT_RETURNTRANSFER => true,
+        CURLOPT_ENCODING => '',
+        CURLOPT_MAXREDIRS => 10,
+        CURLOPT_TIMEOUT => 0,
+        CURLOPT_FOLLOWLOCATION => true,
+        CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+        CURLOPT_CUSTOMREQUEST => 'GET',
+        CURLOPT_HTTPHEADER => array(
+            'X-Sdg-Token: ' . $this->api_token
+        ),
+        ));
+
+        $response = curl_exec($curl);
+        curl_close($curl);
+        return jso_decode($response);
+    }
     public function updateSensor()
     {
         $devices = $this->getAlldevices();
         foreach($devices as $device)
         {
-            var_dump($device);
+            $deviceData = getDeviceData($device->DeviceID);
+
+            $dbdata = array(
+                'deviceId' => $device->DeviceID,
+                'type' => 'battvolt',
+                'observationId' => null,
+                'tag' => $device->GroupName ?? '',
+                'name' => $device->FriendlyName ?? '',
+                'unit' => '',
+                'value' => $deviceData[0]->battvolt,
+                'message_time' => $device->Time,
+            );
+            var_dump($dbdata);
         }
     }
 }
