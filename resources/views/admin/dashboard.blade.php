71313b3b7d0b62096d63f3dc25d9e62fdba90e5c
Ivan Orlando <smartdevello@outlook.com>
2022-09-01 13:26:28 -0400

    curly brace

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -103,7 +103,7 @@
 
                                 <template v-slot:expanded-item="{ headers, item }">
                                     <td :colspan="headers.length">
-                                        <div v-pre>@{{ item.logs }}</div>
+                                        <div v-pre>"{ item.logs }"</div>
                                     </td>
                                   </template>
 
