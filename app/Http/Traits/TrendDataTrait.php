ce27e73e0c7be7c1f7ea4cbaf6c2e4ee4a3de732
Ivan Orlando <smartdevello@outlook.com>
2022-10-13 12:43:55 -0400

    log all payload content

--- app/Http/Traits/TrendDataTrait.php
+++ app/Http/Traits/TrendDataTrait.php
@@ -182,7 +182,7 @@ trait TrendDataTrait
         // Write the contents to the file, 
         // using the FILE_APPEND flag to append the content to the end of the file
         // and the LOCK_EX flag to prevent anyone else writing to the file at the same time
-        file_put_contents('logs.txt', $msg, FILE_APPEND | LOCK_EX);        
+        file_put_contents('logs.txt', json_encode( $payload ) . " " . $httpcode .  "\n", FILE_APPEND | LOCK_EX);        
 
         return $csv_data;
     }
