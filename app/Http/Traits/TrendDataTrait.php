e0e91be1dccf8c02ed38bec475c2c34d44b24152
Ivan Orlando <smartdevello@outlook.com>
2022-10-13 10:22:52 -0400

    log every fressi files

--- app/Http/Traits/TrendDataTrait.php
+++ app/Http/Traits/TrendDataTrait.php
@@ -7,6 +7,8 @@ use App\Models\Csv_Trend_Data;
 use Illuminate\Support\Facades\Storage;
 use DateTime;
 use stdClass;
+use TheSeer\Tokenizer\Exception;
+
 
 trait TrendDataTrait
 {
@@ -58,10 +60,15 @@ trait TrendDataTrait
     }
     public function receive_csv_save_db($trend_group)
     {
-                
-        $filename = sprintf("trend_group_%d.csv", $trend_group->id);
+        $payload = new stdClass();
+        $payload ->{"message#"} = time() - 1665428000;
+        $payload -> currentTime = date_format(new DateTime(), 'Y-m-d H:i:s.vO');
+        $payload->measurementPoint = array();
+
         $to_time = time();
         $from_time = $to_time - $trend_group->query_period * 60;
+        $filename = sprintf("%s_%s.csv", $trend_group->trend_group_name, $payload->currentTime );
+
 
         //Convet it to milisecond;
         $from_time *=1000;
@@ -70,98 +77,102 @@ trait TrendDataTrait
         $format = "lynx --dump 'http://172.21.8.245/COSMOWEB?TYP=REGLER&MSG=GET_TRENDVIEW_DOWNLOAD_CVS&COMPUTERNR=THIS&REGLERSTRANG=%s&REZEPT=%s&FROMTIME=%d&TOTIME=%d&' > " . $filename ;
         $command = sprintf($format, $trend_group->controller_id, $trend_group->trend_group_name, $from_time, $to_time);
 
-        if ( file_exists($filename ) ) {
-            unlink($filename);
-        }
-        shell_exec($command);
-
-        $file = fopen($filename,"r");
-        $csv_data = [];
-        $index = 0;
-        while(! feof($file))
-        {
-            $index++;
-            $row = fgetcsv($file, 0, ';');
-            if (is_array($row)) 
-                $csv_data[] = $row;         
-        }
-        fclose($file);
-
-        if ( strpos( $trend_group->trend_group_name, "Freesi") !== false)  {
-            if ( count($csv_data) >= 3) {
-                $columns = $csv_data[0];
-                $values = $csv_data[count($csv_data) - 1];
-                $payload = new stdClass();
-                $payload ->{"message#"} = time() - 1665428000;
-                $payload -> currentTime = date_format(new DateTime(), 'Y-m-d H:i:s.vO');
-                $payload->measurementPoint = array();
-                for ($i = 0; $i < count($values); $i++) {
-                    if ( preg_match("/^[\d,]+$/", $values[$i]) ) {
-                        $payload->measurementPoint[] = (object) array(
-                            'controller'=>$trend_group->trend_group_name,
-                            'pointName'=> $this->convertFinnishtoEnglish ( $columns[$i] ),
-                            'out'=> (float)str_replace(",", ".", $values[$i]),
-                            'facet' => '',
-                        );
-                    }                    
-                }
-
-                $curl = curl_init();
-
-                curl_setopt_array($curl, array(
-                CURLOPT_URL => 'https://freesi-sensor-data-hub.azure-devices.net/devices/hka_vipusenkatu5a/messages/events?api-version=2018-06-30',
-                CURLOPT_RETURNTRANSFER => true,
-                CURLOPT_ENCODING => '',
-                CURLOPT_MAXREDIRS => 10,
-                CURLOPT_TIMEOUT => 0,
-                CURLOPT_FOLLOWLOCATION => true,
-                CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-                CURLOPT_CUSTOMREQUEST => 'POST',
-                CURLOPT_POSTFIELDS => json_encode( $payload ),
-                CURLOPT_HTTPHEADER => array(
-                    'authorization: SharedAccessSignature sr=freesi-sensor-data-hub.azure-devices.net%2Fdevices%2Fhka_vipusenkatu5a&sig=BIW3fIdoAL70h6tw0Bj0dyXFrd%2BIg7eN3ctYZZnNltc%3D&se=2024431159',
-                    'Content-Type: application/json'
-                ),
-                ));
-
-                $response = curl_exec($curl);
-                curl_close($curl);
+        try{
+    
+            if ( file_exists($filename ) ) {
+                unlink($filename);
             }
-                
+            shell_exec($command);
+    
+            $file = fopen($filename,"r");
+            $csv_data = [];
+            $index = 0;
+            while(! feof($file))
+            {
+                $index++;
+                $row = fgetcsv($file, 0, ';');
+                if (is_array($row)) 
+                    $csv_data[] = $row;         
+            }
+            fclose($file);
+    
+            if ( strpos( $trend_group->trend_group_name, "Freesi") !== false)  {
+                if ( count($csv_data) >= 3) {
+                    $columns = $csv_data[0];
+                    $values = $csv_data[count($csv_data) - 1];
+    
+                    for ($i = 0; $i < count($values); $i++) {
+                        if ( preg_match("/^[\d,]+$/", $values[$i]) ) {
+                            $payload->measurementPoint[] = (object) array(
+                                'controller'=>$trend_group->trend_group_name,
+                                'pointName'=> $this->convertFinnishtoEnglish ( $columns[$i] ),
+                                'out'=> (float)str_replace(",", ".", $values[$i]),
+                                'facet' => '',
+                            );
+                        }                    
+                    }
+    
+                    $curl = curl_init();
+    
+                    curl_setopt_array($curl, array(
+                    CURLOPT_URL => 'https://freesi-sensor-data-hub.azure-devices.net/devices/hka_vipusenkatu5a/messages/events?api-version=2018-06-30',
+                    CURLOPT_RETURNTRANSFER => true,
+                    CURLOPT_ENCODING => '',
+                    CURLOPT_MAXREDIRS => 10,
+                    CURLOPT_TIMEOUT => 0,
+                    CURLOPT_FOLLOWLOCATION => true,
+                    CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+                    CURLOPT_CUSTOMREQUEST => 'POST',
+                    CURLOPT_POSTFIELDS => json_encode( $payload ),
+                    CURLOPT_HTTPHEADER => array(
+                        'authorization: SharedAccessSignature sr=freesi-sensor-data-hub.azure-devices.net%2Fdevices%2Fhka_vipusenkatu5a&sig=BIW3fIdoAL70h6tw0Bj0dyXFrd%2BIg7eN3ctYZZnNltc%3D&se=2024431159',
+                        'Content-Type: application/json'
+                    ),
+                    ));
+    
+                    $response = curl_exec($curl);
+                    curl_close($curl);
+                }
+                    
+            }
+    
+            $output = [];
+            // foreach($csv_data as $index => $arr)
+            // {
+            //     if ($index !=0) {
+            //         $timestamp = 0;
+    
+            //         if ( count($arr) < 3 ) continue;
+            //         foreach($arr as $key => $value) {
+    
+            //             if ($key == 1){
+            //                 $timestamp = strtotime( trim($arr[0]). " " . trim($arr[1]));
+            //             } else if ($key !=0 && $key !=1) {
+            //                 if ( empty( $csv_data[0][$key] ) || empty( $value ) ) continue;
+     
+            //                 $data = [
+            //                     'trend_group_id' => $trend_group->id,
+            //                     'timestamp' => date('Y-m-d H:i:s', $timestamp),
+            //                     'sensor_name' => $csv_data[0][$key],
+            //                     'sensor_value' => number_format(round( floatval ( str_replace(",", ".", $value) ), 1)  , 1, '.', '')
+            //                 ];
+            //                 $csv_trend_data = Csv_Trend_Data::updateOrCreate(
+            //                     ['trend_group_id'=> $trend_group->id, 'sensor_name' => $csv_data[0][$key]], $data
+            //                 );
+    
+            //                 if ($csv_trend_data)
+            //                     $output[] = $csv_trend_data;
+    
+            //             }
+            //         }
+            //     }
+            // }
+    
+            return $csv_data;
+        }catch(Exception $e) {            
+            $msg = sprintf("%s               %s.csv", $filename, $e->getMessage() );
+            $my_log = file_put_contents('logs.txt', $msg.PHP_EOL, FILE_APPEND | LOCK_EX );
         }
-
-        $output = [];
-        // foreach($csv_data as $index => $arr)
-        // {
-        //     if ($index !=0) {
-        //         $timestamp = 0;
-
-        //         if ( count($arr) < 3 ) continue;
-        //         foreach($arr as $key => $value) {
-
-        //             if ($key == 1){
-        //                 $timestamp = strtotime( trim($arr[0]). " " . trim($arr[1]));
-        //             } else if ($key !=0 && $key !=1) {
-        //                 if ( empty( $csv_data[0][$key] ) || empty( $value ) ) continue;
- 
-        //                 $data = [
-        //                     'trend_group_id' => $trend_group->id,
-        //                     'timestamp' => date('Y-m-d H:i:s', $timestamp),
-        //                     'sensor_name' => $csv_data[0][$key],
-        //                     'sensor_value' => number_format(round( floatval ( str_replace(",", ".", $value) ), 1)  , 1, '.', '')
-        //                 ];
-        //                 $csv_trend_data = Csv_Trend_Data::updateOrCreate(
-        //                     ['trend_group_id'=> $trend_group->id, 'sensor_name' => $csv_data[0][$key]], $data
-        //                 );
-
-        //                 if ($csv_trend_data)
-        //                     $output[] = $csv_trend_data;
-
-        //             }
-        //         }
-        //     }
-        // }
-
-        return $csv_data;
+        return null;
     }
 }
