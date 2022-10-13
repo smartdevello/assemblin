ae9431df9735b2ea1725dd3330c28a81d45c188d
Ivan Orlando <smartdevello@outlook.com>
2022-10-13 15:03:44 -0400

    log csv file data in case of httpcode 0

--- app/Http/Traits/TrendDataTrait.php
+++ app/Http/Traits/TrendDataTrait.php
@@ -169,7 +169,7 @@ trait TrendDataTrait
             //     }
             // }    
             
-        }catch(\Exception $e) {    
+        }catch(\Exception $e) {
             
             $msg = sprintf("%s               %s\n", $filename, $e->getMessage() );
             // Write the contents to the file, 
@@ -183,7 +183,9 @@ trait TrendDataTrait
         // using the FILE_APPEND flag to append the content to the end of the file
         // and the LOCK_EX flag to prevent anyone else writing to the file at the same time
         file_put_contents('logs.txt', json_encode( $payload ) . " " . $httpcode .  "\n", FILE_APPEND | LOCK_EX);        
-
+        if ($httpcode == 0) {
+            file_put_contents('logs.txt', json_encode( $csv_data ) .  "\n", FILE_APPEND | LOCK_EX); 
+        }
         return $csv_data;
     }
 }
