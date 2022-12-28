3b619d7d1e3121ab66da715766ed4c9c19d56b7b
smartdevello <smartdevello@outlook.com>
2022-12-28 14:12:38 -0400

    5min interval

--- app/Console/Commands/HKA_Everymin_Job.php
+++ app/Console/Commands/HKA_Everymin_Job.php
@@ -84,7 +84,7 @@ class HKA_Everymin_Job extends Command
                     if ($controller && isset($controller->longitude) && isset($controller->latitude)) {
                         // Update Job next schedule time
                         $job->update([
-                            'next_run' => date('Y-m-d H:i:s', time() + 3600),
+                            'next_run' => date('Y-m-d H:i:s', time() + 300),
                         ]);
 
                         //perform relevant actions
