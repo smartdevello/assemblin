7ecd2b9f51980268752d7d54ec095d22ebdaa9ca
smartdevello <smartdevello@outlook.com>
2023-12-05 14:16:40 -0500

    return cmdresult

--- app/Http/Traits/TrendDataTrait.php
+++ app/Http/Traits/TrendDataTrait.php
@@ -37,8 +37,8 @@ trait TrendDataTrait
             $to_time *= 1000;
 
             $format = "lynx --dump 'http://172.21.8.245/COSMOWEB?TYP=REGLER&MSG=GET_TRENDVIEW_DOWNLOAD_CVS&COMPUTERNR=THIS&REGLERSTRANG=%s&REZEPT=%s&FROMTIME=%d&TOTIME=%d&' > " . $local_folderpath . $local_filename;                      
-            $cmdresult = $command = sprintf($format, $trend_group->controller_id, $trend_group->trend_group_name, $from_time, $to_time);
-            shell_exec($command);
+            $command = sprintf($format, $trend_group->controller_id, $trend_group->trend_group_name, $from_time, $to_time);
+            $cmdresult = shell_exec($command);
             $local_storage_path = str_replace(" ", "_", sprintf("%s/%s%s%s.csv", $date, $now, $trend_group->trend_group_name, $trend_group->controller_id));
 
 
