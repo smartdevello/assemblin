5bb55c8d21a14c111ef1c52a13eb6b2eebfacad7
smartdevello <smartdevello@outlook.com>
2024-09-18 10:39:27 -0400

    undefined val

--- resources/views/admin/zenner/index.blade.php
+++ resources/views/admin/zenner/index.blade.php
@@ -37,28 +37,6 @@
                                     nextIcon: 'mdi-plus'
                                 }"
                             >
-                                <!-- <template v-slot:expanded-item="{ headers, item }">
-                                    <v-simple-table>
-                                        <template v-slot:default>
-                                          <tbody class="log_table">
-                                            <tr>
-                                                <td class="table_header">DateTime<td>
-                                                <td v-for="(i, val) in item.logs" :key="val" class="table_value">
-                                                    {{val}}
-                                                </td>
-                                            </tr>
-                                            <tr>
-                                                <td class="table_header">Value<td>
-                                                <td v-for="(i, val) in item.logs" :key="val" class="table_value">
-                                                    {{i}}
-                                                </td>
-                                            </tr>
-
-                                          </tbody>
-                                        </template>
-                                      </v-simple-table>
-                                  </template> -->
-
                             </v-data-table>
                 </v-card>
             </template>
